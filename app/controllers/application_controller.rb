686629dd6ee6668473a116f19086d98990f21597
tanasashi <waka_tan_212@ezweb.ne.jp>
2021-10-23 14:06:13 +0000

    67

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,9 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  Refile.secret_key = '9c593fdc88913eb859715738a4ecd5f7e5d115baf3de48024f910cfd3b7767d6a4fc67738f8dcdc0b2ce302fb4b8905f84ad19c45fe43d2773e37fa7ec4a8c0a'
 
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
-Post.create!()
+Post.create!(
+  user_id: 3,
+  area_id: 1,
+  image: File.open("./app/assets/images/image0.jpg"),
+  place_name: '公園',
+  address: '札幌市111',
+  detail: '北海道でで5番目に広い公園です。'
+)
+
+
+
