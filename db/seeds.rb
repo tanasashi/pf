8c133f47d3ba2f22d5fc7e43e70ac34a3b75b661
tanasashi <waka_tan_212@ezweb.ne.jp>
2021-10-24 15:06:05 +0000

    test

--- db/seeds.rb
+++
@@ -1,9 +0,0 @@
-Post.create!(
-  user_id: 16,
-  area_id: 1,
-  image: File.open("./app/assets/images/seeds/seeds1.jpg"),
-  place_name: 'すすきの歓楽街',
-  address: '札幌市中央区1222',
-  detail: '札幌でのみに行くならここ！'
-)
-
